632eaf4cb9ac185104d2f17074b07f34218f7dad
陈文欢 <1406269886@qq.com>
2020-06-30 14:08:01 +0800

    引入element ui

---
+++ src/views/login/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login">1231</div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="scss" scoped>
+.login {
+  background: #344a5f;
+  height: 100vh;
+}
+</style>
