5eec8a77c8e68167251693eb53e96abc8376adc2
wen <l>
2021-07-04 05:48:17 +0800

    创建项目

---
+++ src/views/Login/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="login">
+    <el-button type="primary" plain>主要按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
